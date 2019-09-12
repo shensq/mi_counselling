19041e849fee250e5598d4b267035fe9a059d0f5
shensq <shensq@umich.edu>
2019-09-12 14:49:48 -0400

    include bash

--- code/run.sh
+++ code/run.sh
@@ -9,16 +9,27 @@ pwd
 # CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir SR_only --num_train_epochs 5
 # CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir SR_only --output_dir SR_only
 
-mkdir -p ../../models/Smoking_only
-CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir Smoking_only --num_train_epochs 5 
-CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir Smoking_only --output_dir Smoking_only
+# mkdir -p ../../models/Smoking_only
+# CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir Smoking_only --num_train_epochs 5 
+# CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir Smoking_only --output_dir Smoking_only
 
-mkdir -p ../../models/Diet_only
-CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir Diet_only --num_train_epochs 5 
-CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir Diet_only --output_dir Diet_only
+# mkdir -p ../../models/Diet_only
+# CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir Diet_only --num_train_epochs 5 
+# CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir Diet_only --output_dir Diet_only
 
-# mkdir -p ../../models/mi_tuned
-# CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir mi_tuned --num_train_epochs 5
-# CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir mi_tuned --output_dir mi_tuned
+# mkdir -p ../../models/mi_tuned_5epo
+# CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir mi_tuned_5epo --num_train_epochs 5
+# CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir mi_tuned_5epo --output_dir mi_tuned_5epo
 
+# mkdir -p ../../models/mi_tuned_15epo
+# CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir mi_tuned_15epo --num_train_epochs 15
+# CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir mi_tuned_15epo --output_dir mi_tuned_15epo
+# CUDA_VISIBLE_DEVICES=2 python retrieve_candidate.py
+# mkdir -p ../../models/mi_tuned_aug
+# CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir mi_tuned_aug --num_train_epochs 10 --augment
+# CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir mi_tuned_aug --output_dir mi_tuned_aug --augment
+
+mkdir -p ../../models/mi_tuned_aug_15epo
+CUDA_VISIBLE_DEVICES=2 python gpt_tuning.py --output_dir mi_tuned_aug_15epo --num_train_epochs 15 --augment
+CUDA_VISIBLE_DEVICES=2 python gpt_sample.py --model_dir mi_tuned_aug_15epo --output_dir mi_tuned_aug_15epo --augment
 echo 'Finished.'
