b334b1e6b663cf2d85d771b890594c95d85993d7
shensq <shensq@umich.edu>
2019-09-12 20:57:32 -0400

    deeplearn server save

--- code/model/my_model.py
+++ code/model/my_model.py
@@ -9,7 +9,9 @@ class GPT2ClassHeadsModel(GPT2Model):
         super(GPT2ClassHeadsModel, self).__init__(config)
         self.transformer = GPT2Model(config)
         
-        self.classifier = nn.Linear(config.n_embd, 2, bias=False)
+        self.classifier = nn.Linear(config.n_embd, 2)
+        # self.classifier = nn.Sequential(nn.Linear(config.n_embd, 300), nn.Tanh(), nn.Dropout(p=0.1), 
+        #     nn.Linear(300, 300), nn.Tanh(), nn.Dropout(p=0.1), nn.Linear(300, 2))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         self.apply(self.init_weights)
@@ -26,8 +28,7 @@ class GPT2ClassHeadsModel(GPT2Model):
         transformer_outputs = self.transformer(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,
                                                past=past, head_mask=head_mask)
         hidden_states = transformer_outputs[0] # torch.Size([1, 124, 1024])
-        
-        logits = self.classifier(hidden_states[:,0,:]) # torch.Size([1,2])
+        logits = self.classifier(hidden_states[:,-1,:]) # torch.Size([1,2])
         loss_fct = CrossEntropyLoss()
         loss = loss_fct(logits,labels)
         
