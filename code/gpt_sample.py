fb387d443777be8e5d023e0566d6182b4f280da1
Siqi Shen <shensq@umich.edu>
2019-09-20 00:05:32 -0400

    sampling now ignore the past. new version of sampling function from pytorch-transformer library

--- code/gpt_sample.py
+++ code/gpt_sample.py
@@ -51,7 +51,81 @@ def get_topic_keywords(meta):
         keywords_down += [6551, 4483, 2057, 9799, 4425, 4461, 4255, 5517]
     return keywords_up, keywords_down
 
+
+def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')):
+    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+        Args:
+            logits: logits distribution shape (vocabulary size)
+            top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+            top_p > 0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+        From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+    assert logits.dim() == 1  # batch size 1 for now - could be updated for more but the code would be less clear
+    top_k = min(top_k, logits.size(-1))  # Safety check
+    if top_k > 0:
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        logits[indices_to_remove] = filter_value
+
+    if top_p > 0.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold
+        sorted_indices_to_remove = cumulative_probs > top_p
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        indices_to_remove = sorted_indices[sorted_indices_to_remove]
+        logits[indices_to_remove] = filter_value
+    return logits
+
+def sample_sequence_new(model, length, context, start_token=None, batch_size=1, modified_decoding=False,
+                        value_word_relation=None, meta=None, key_word=None, num_samples=1, temperature=1,
+                        top_k=0, top_p=0.0, device='cuda'):
+    context = torch.tensor(context, dtype=torch.long, device=device)
+    context = context.unsqueeze(0).repeat(num_samples, 1)
+    generated = context
+    prev = context
+    past = None
+
+    with torch.no_grad():
+        for _ in trange(length):
+            inputs = {'input_ids': generated, 'past': None, 'key_word': key_word}
+            logits, past = model(**inputs)
+            next_token_logits = logits[0, -1, :] / temperature
+            filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
+            next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+            generated = torch.cat((generated, next_token.unsqueeze(0)), dim=1)
+            prev = next_token.unsqueeze(0)
+            if next_token[0] in [50256]:
+                break
+    return generated
+
+
 def sample_sequence(model, length, start_token=None, batch_size=None, context=None, temperature=1, top_k=0, modified_decoding=False,value_word_relation=None,device='cuda', sample=True,meta=None, key_word=None):
+    """ Generating a sequence until reaching the maximum length or getting an <eos> token.
+
+    The generation is in an auto-regressive way. The initial input is the former utterances, w/o retrieved sentences or
+    phrases.
+
+    :param model: The PyTorch model to be used.
+    :param length: The maximum decoding length
+    :param start_token:
+    :param batch_size:
+    :param context: Tensor. The word tokens that the generation conditioned on.
+    :param temperature: Double. The lower the temperature, the more likely the token with the highest probability is picked.
+    :param top_k: Int. Sample from only the top-k probabilities to avoid unexpected result due to randomness
+    :param modified_decoding: Boolean, whether is modify the prob based on lexicon.
+    :param value_word_relation: Tuple. Contains word-'value lexicon' relationship.
+    :param device: String. Specify the device to put the model for PyTorch
+    :param sample: Boolean. To do sampling or not.
+    :param meta: Tuple. Contains the meta information of the current input sample.
+    :param key_word: A list of key phrases that will be used for cross-attention decoding.
+    :return: TODO what is the output?
+    """
     if start_token is None:
         assert context is not None, 'Specify exactly one of start_token and context!'
         context = torch.tensor(context, device=device, dtype=torch.long).unsqueeze(0).repeat(batch_size, 1)
@@ -82,8 +156,8 @@ def sample_sequence(model, length, start_token=None, batch_size=None, context=No
     with torch.no_grad():
         for i in trange(length):
             logits, past = model(prev, past=past, key_word=key_word)
-            logits = logits[:, -1, :] / temperature # torch.Size([1, 50257])
-            logits = top_k_logits(logits, k=top_k) 
+            logits = logits[:, -1, :] / temperature # torch.Size([1, 50257]). next_token_logits
+            logits = top_k_logits(logits, k=top_k)  # filtered_logits
             log_probs = F.softmax(logits, dim=-1) # torch.Size([1, 50257])
             #=== modify probability after softmax ==========
             if modified_decoding:
@@ -99,7 +173,7 @@ def sample_sequence(model, length, start_token=None, batch_size=None, context=No
                         
             #===================
             if sample:
-                prev = torch.multinomial(log_probs, num_samples=1) # no need to normalize
+                prev = torch.multinomial(log_probs, num_samples=1) # no need to normalize. sample the next token. (1,1)
                 # import pdb;pdb.set_trace()
                 prev_token = prev[0][0].tolist()
                 if prev_token in word2value:    
@@ -111,7 +185,7 @@ def sample_sequence(model, length, start_token=None, batch_size=None, context=No
 
             else:
                 _, prev = torch.topk(log_probs, k=1, dim=-1)
-            output = torch.cat((output, prev), dim=1)
+            output = torch.cat((output, prev), dim=1) # con
             if prev[0][0] in [50256]:
                 break
     return output
@@ -130,7 +204,7 @@ def run_model():
     parser.add_argument('--modified_decoding', action='store_true')
     parser.add_argument('--augment', action='store_true')
     parser.add_argument('--special_input',type=str,default='x_y_meta')
-    parser.add_argument('--keyword', action='store_true')
+    parser.add_argument('--keyword', action='store_true',default=True)
     args = parser.parse_args()
     print(args)
 
@@ -147,12 +221,12 @@ def run_model():
     # ====== Load GPT2 model ========
     # TODO: use the new data, store to another file
     model_dir = '/data/chuancen/LIT/models/'+args.model_dir
-    model = GPT2LMHeadModel.from_pretrained(model_dir)
-    # model = GPT2LMHeadModel.from_pretrained('gpt2')
+    # model = GPT2LMHeadModel.from_pretrained(model_dir)
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
     if USE_CUDA:
         model.cuda()
-    tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
-    # tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    # tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     
     # ========== Prepare lexicon =============
     value2word, word2value = values_lexicon_encode(path='../data_processed/values_lexicon/values_lexicon.txt',tokenizer=tokenizer)
@@ -165,8 +239,8 @@ def run_model():
         gpt_data = GptDataset_aug(x_y_meta,tokenizer) # use the name of output, it is depend on how is the trained model
     elif args.keyword:
         print("Using keyword cross attention")
-        pickle_handler = open('/data/chuancen/LIT/mi_counselling/data_processed/x_y_meta_keyword', 'rb')
-        # pickle_handler = open('/Users/shensq/Google Drive/Research/mi_counselling/data_processed/x_y_meta_keyword', 'rb')
+        # pickle_handler = open('/data/chuancen/LIT/mi_counselling/data_processed/x_y_meta_keyword', 'rb')
+        pickle_handler = open('/Users/shensq/Google Drive/Research/mi_counselling/data_processed/x_y_meta_keyword', 'rb')
         x_y_meta = pickle.load(pickle_handler)
         gpt_data = GptDataset_keyword(x_y_meta, tokenizer)
     else:
@@ -227,7 +301,7 @@ def run_model():
             decode_length = int(len(context_tokens))
             # if args.augment:
             #     decode_length = int(0.5 * (5/6) * len(context_tokens))
-            out = sample_sequence(
+            out = sample_sequence_new(
                 # model=model, length=args.length,
                 model=model,length=decode_length,
                 context=context_tokens,
