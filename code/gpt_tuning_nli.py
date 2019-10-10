95083762ad5fcfbad4429b649e87bc05d5bc3714
Siqi Shen <shensq@umich.edu>
2019-10-10 16:08:32 -0400

    updated parameters and learning rate change for nli tuning

--- code/gpt_tuning_nli.py
+++ code/gpt_tuning_nli.py
@@ -47,10 +47,10 @@ def main():
     parser.add_argument("--output_dir", default='mi_tuned', type=str, required=False,
                         help="The output directory where the model predictions and checkpoints will be written.")
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--num_train_epochs', type=int, default=3)
-    parser.add_argument('--train_batch_size', type=int, default=1)
+    parser.add_argument('--num_train_epochs', type=int, default=5)
+    parser.add_argument('--train_batch_size', type=int, default=2)
     parser.add_argument('--max_grad_norm', type=int, default=1)
-    # parser.add_argument('--learning_rate', type=float, default=6.25e-5)
+    # parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--learning_rate', type=float, default=6.25e-5)
     parser.add_argument('--warmup_proportion', type=float, default=0.1)
     parser.add_argument('--lr_schedule', type=str, default='warmup_linear')
@@ -73,29 +73,28 @@ def main():
 
     
     # ====== Load GPT2 model ========
-    model_dir = '/data/chuancen/LIT/models/'+args.model_dir
-    model = GPT2ClassHeadsModel.from_pretrained(model_dir)
-    # model = GPT2ClassHeadsModel.from_pretrained('gpt2')
+    model_dir = "/Users/shensq/Documents/LIT_ai_counseling/gpt2/models/pytorch_345M/"
+    # model = GPT2ClassHeadsModel.from_pretrained(model_dir)
+    model = GPT2ClassHeadsModel.from_pretrained('gpt2')
     if USE_CUDA:
         model.cuda()
     tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
     # tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     print('Model loaded.')
     # =============== Load & process data ==============
-    # pickle_handler = open('/data/chuancen/LIT/mi_counselling/data_processed/x_y_meta','rb')
     pickle_handler = open('../data_processed/x_y_meta', 'rb')
     x_y_meta = pickle.load(pickle_handler)
     if args.snli:
         gpt_data = SnliDataset(tokenizer)  # use the output model name as pattern name
     else:
         gpt_data = GptDataset_nli(x_y_meta, tokenizer, augment=True)
-    
+
     print("Dataset initialized.")
 
-    test_size  = int(len(gpt_data)*0.10)
-    val_size = int(len(gpt_data)*0.05)
+    test_size  = int(len(gpt_data)*0.90)
+    val_size = int(len(gpt_data)*0.095)
     gpt_train,gpt_test,gpt_val = torch.utils.data.random_split(gpt_data,[len(gpt_data)-test_size-val_size,test_size,val_size])
-    
+
     data_loader = DataLoader(dataset=gpt_train,batch_size=args.train_batch_size,shuffle=True,drop_last=True,collate_fn=collate_fn_nli)
     test_loader = DataLoader(dataset=gpt_test, batch_size=1, shuffle=True, drop_last=True, collate_fn=collate_fn_nli)
 
@@ -108,14 +107,15 @@ def main():
     # import pdb;pdb.set_trace()
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if n[:10]=='classifier'], 'weight_decay': 0.01},
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay) and n[:10]!='classifier'], 'weight_decay': 0.01},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay) and n[:10]!='classifier'], 'weight_decay': 0.0}
+        {'params': [p for n, p in param_optimizer if 'classifier' in n and 'bias' not in n], 'weight_decay': 0.01, 'lr':1e-3},
+        {'params': [p for n, p in param_optimizer if 'classifier' in n and 'bias' in n], 'weight_decay': 0.00,'lr': 1e-3},
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay) and 'classifier' not in n], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay) and 'classifier' not in n], 'weight_decay': 0.0}
         ]
     # optimizer_grouped_parameters = [{'params': [p for n, p in param_optimizer if n=='classifier.weight'], 'weight_decay': 0.01,'lr':1e-4}]
 
     num_train_optimization_steps = len(gpt_train) * args.num_train_epochs // args.train_batch_size
-    num_warmup_steps = int(num_train_optimization_steps * 0.1)
+    num_warmup_steps = 400
 
     optimizer = AdamW(optimizer_grouped_parameters,lr=args.learning_rate,correct_bias=True)
     # scheduler = pytorch_transformers.optimization.WarmupCosineSchedule(optimizer, warmup_steps=num_warmup_steps, t_total=num_train_optimization_steps,cycles=1.5)
@@ -132,24 +132,30 @@ def main():
     model.train()
     exp_average_loss = None
     counter=0
-    
+
     for epo in trange(int(args.num_train_epochs), desc="Epoch"):
         tqdm_bar = tqdm(data_loader, desc="Training")
         accuracy = 0 
         for x,type_x,pos_x,lm_x,label in data_loader:
             if counter>0:
                 break
+            for i in range(x.shape[0]):
+                if label[i].item()==0:
+                    x[i,:].fill_(0)
+                else:
+                    x[i,:].fill_(1)
             # counter+=1
             # print("Get data")
             loss,logits = model(x, position_ids=pos_x, token_type_ids=type_x, labels=label)
             # print("Forward pass")
             # loss.backward(torch.ones(2).cuda())
-            if torch.argmax(logits,dim=1).item()==label.item():
-                accuracy+=1
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+            pred = torch.argmax(logits, dim=1)
+            for i in range(x.shape[0]):
+                if pred[i].item() == label[i].item():
+                    accuracy += 1
             loss.backward()
-            scheduler.step()
             optimizer.step()
+            scheduler.step()
             # print("loss BP")
             optimizer.zero_grad()
             exp_average_loss = loss.item() if exp_average_loss is None else 0.7*exp_average_loss+0.3*loss.item()
@@ -160,18 +166,19 @@ def main():
             # print(exp_average_loss)
         accuracy/=len(gpt_train)
         print("Accuracy for epoch {} is {}".format(epo,accuracy))
-        # ==== Save the model ====
-        # Save a trained model, configuration and tokenizer
-        model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
-
-        # If we save using the predefined names, we can load using `from_pretrained`
-        output_dir = '/data/chuancen/LIT/models/'
-        output_model_file = os.path.join(output_dir+args.output_dir, WEIGHTS_NAME)
-        output_config_file = os.path.join(output_dir+args.output_dir, CONFIG_NAME)
-
-        torch.save(model_to_save.state_dict(), output_model_file)
-        model_to_save.config.to_json_file(output_config_file)
-        tokenizer.save_vocabulary(output_dir+args.output_dir)
+        #
+        # # ==== Save the model ====
+        # # Save a trained model, configuration and tokenizer
+        # model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
+        #
+        # # If we save using the predefined names, we can load using `from_pretrained`
+        # output_dir = '/data/chuancen/LIT/models/'
+        # output_model_file = os.path.join(output_dir+args.output_dir, WEIGHTS_NAME)
+        # output_config_file = os.path.join(output_dir+args.output_dir, CONFIG_NAME)
+        #
+        # torch.save(model_to_save.state_dict(), output_model_file)
+        # model_to_save.config.to_json_file(output_config_file)
+        # tokenizer.save_vocabulary(output_dir+args.output_dir)
 
 
 if __name__ == '__main__':
--- code/model/my_model.py
+++ code/model/my_model.py
@@ -1,27 +1,21 @@
 import pytorch_transformers
-from pytorch_transformers import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel,AdamW, WEIGHTS_NAME, CONFIG_NAME
+from pytorch_transformers import GPT2PreTrainedModel, GPT2Model, GPT2LMHeadModel,AdamW, WEIGHTS_NAME, CONFIG_NAME
 import torch
 import torch.nn as nn
 from torch.nn import CrossEntropyLoss
 
-class GPT2ClassHeadsModel(GPT2Model):
+class GPT2ClassHeadsModel(GPT2PreTrainedModel):
     def __init__(self, config):
         super(GPT2ClassHeadsModel, self).__init__(config)
         self.transformer = GPT2Model(config)
         
         self.classifier = nn.Linear(config.n_embd, 2)
-        # self.classifier = nn.Sequential(nn.Linear(config.n_embd, 768), nn.Tanh(), nn.Dropout(p=0.1),
+        # self.classifier = nn.Sequential(nn.Linear(config.n_embd, 768), nn.ReLU(), nn.Dropout(p=0.2),
         #                                 nn.Linear(768, 2))
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        # self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-        self.apply(self.init_weights)
+        self.init_weights()
 
-    def tie_weights(self):
-        """ Make sure we are sharing the input and output embeddings.
-            Export to TorchScript can't handle parameter sharing so we are cloning them instead.
-        """
-        self._tie_or_clone_weights(self.lm_head,
-                                   self.transformer.wte)
 
     def forward(self, input_ids, labels=None, token_type_ids=None,
                 position_ids=None, past=None, head_mask=None):
