f9c13cbc9a236bd169de576cc363da2fc8bec95e
Siqi Shen <shensq@umich.edu>
2019-10-13 16:12:35 -0400

    the lambdaLR now support the new parameter groups

--- code/utils/my_optim.py
+++ code/utils/my_optim.py
@@ -1,4 +1,5 @@
-def get_unfreezing_funcs(optimizer_grouped_parameters, warmup_portion, total_steps, use_unfreezing=True):
+def get_unfreezing_funcs(optimizer_grouped_parameters, warmup_portion, total_steps, use_unfreezing=False):
+
     def get_lr_lambda(block, use_unfreezing):
         if use_unfreezing:
             NUM_STAGES = 50
@@ -20,7 +21,10 @@ def get_unfreezing_funcs(optimizer_grouped_parameters, warmup_portion, total_ste
     lm_funcs = []
     for group in optimizer_grouped_parameters:
         tag = group['tag']
-        if tag[:2] != 'h.':
+        print(tag)
+        if tag == 'wte' or tag == 'wpe':
+            lm_funcs.append(get_lr_lambda(-1, use_unfreezing))
+        elif tag == 'ln_f':
             lm_funcs.append(get_lr_lambda(24, use_unfreezing))
         else:
             try:
@@ -31,7 +35,17 @@ def get_unfreezing_funcs(optimizer_grouped_parameters, warmup_portion, total_ste
     return lm_funcs
 
 
-def construct_grouped_parameters(param_optimizer, learning_rate, use_discr=True):
+def construct_grouped_parameters(param_optimizer, learning_rate, use_discr=False):
+    """
+    Group parameters by the layer they belong to and whether they need weight decay.
+    All the weights requires weight decay (including layer norm). All the biases do not need weight decay.
+    The learning rate diminish by a multiplicative factor for each layer. The layers close to the input will have a
+    smaller learning rate.
+    :param param_optimizer: The collection of named parameters from the model.
+    :param learning_rate: The base learning rate.
+    :param use_discr: Whether to use discriminative learning rate.
+    :return:
+    """
     # discriminative lr
     optimizer_grouped_parameters = []
     weight_decay = 0.01
@@ -40,10 +54,10 @@ def construct_grouped_parameters(param_optimizer, learning_rate, use_discr=True)
     # no_decay = ['bias', 'ln_']
     no_decay = ['bias', 'LayerNorm.weight']
     need_decay = lambda n: not any(nd in n for nd in no_decay)
-    in_block = lambda name, i: "h.{}.".format(i) in name
+    in_block = lambda name, block_id: "h.{}.".format(block_id) in name
 
     if use_discr:
-        embedding_lr = learning_rate * (lr_decay_rate ** num_blocks)
+        embedding_lr = learning_rate * (lr_decay_rate ** (num_blocks+1))
     else:
         embedding_lr = learning_rate
     group = {'params': [p for n, p in param_optimizer if 'wte' in n],
